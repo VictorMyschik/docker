b71743135ed36ef57eabf77ccdcd88fe8afd07eb
Victor <allximik50@gmail.com>
2021-05-12 14:03:17 +0300

    proxy reverse

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Создание обратного прокси
+cd docker/proxy
+docker-compose up -d
+cd ../../
+
+# Создание контейнеров
 docker-compose up -d --build
+
+# Клонирование репозитория
 git clone --recurse-submodules https://github.com/VictorMyschik/mymarket.git www/mymarket
 
+# Копирование файйла настроек для Laravel
 cp www/.env www/mymarket/
