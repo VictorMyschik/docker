2d3fe20a5321ed2ed9fe325667983320f2759c2d
Allximik <allximik50@gmail.com>
2022-07-06 12:04:33 +0300

    install script

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # Создание обратного прокси
-cd docker/proxy && docker-compose up -d
+# shellcheck disable=SC2164
+# Остановить текущий контейнер
+docker-compose down
+
+cd docker/proxy
+# Переустановить сеть
+docker-compose down
+docker-compose up -d
 cd ../../
 
-# Клонирование репозитория
+#docker-compose down && docker-compose build --no-cache
+docker-compose up -d --build
+
+rm -rf www/mymarket
 git clone --recurse-submodules git@github.com:VictorMyschik/mymarket.git www/mymarket
 
 # Копирование файла настроек для Laravel
 cp www/.env www/mymarket/
 
 sudo grep -qxF '127.0.0.1 mymarket.test' /etc/hosts || echo '127.0.0.1 mymarket.test' | sudo tee -a /etc/hosts
-
-# Создание контейнеров
-docker-compose up -d --build
